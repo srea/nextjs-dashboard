1baeea6bb7ba4080b71b3bd5df8e22af3e6deca4
Yuki Tamazawa <yuki.tamazawa@icloud.com>
2025-02-14 00:51:47 +0900

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
-import NavLinks from './dashboard/nav-link';
 
 export default function Page() {
   return (
